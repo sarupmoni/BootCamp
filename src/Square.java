b5fc0854df39f3eac26a9aa728a1c4c7dc82a712
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-05-19 11:24:42 +0530

    implements interfaces

--- src/Square.java
+++ src/Square.java
@@ -1,14 +1,18 @@
-public class Square {
+import models.caluclateCloseShapes;
+
+public class Square implements caluclateCloseShapes {
     private final int side;
 
     public Square(int side) {
         this.side = side;
     }
 
+    @Override
     public int area() {
         return this.side * this.side;
     }
 
+    @Override
     public int perimeter() {
         return 4 * this.side;
     }
