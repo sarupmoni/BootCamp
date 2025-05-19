33ca12834fa6f061cde3b4908560883a6a95168b
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-05-19 10:30:28 +0530

    first commit

---
+++ test/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MainTest {
+    @Test
+    void someTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    void someOther() {
+     assertEquals(1,1);
+    }
+}
